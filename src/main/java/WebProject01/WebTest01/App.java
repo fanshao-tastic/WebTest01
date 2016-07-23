81a48a49751afb59e5cddd6df1538b4bc752326a
fanshao-tastic <907991954@qq.com>
2016-07-23 12:11:42 +0800

    test  DRPC and trident---7.23

---
+++ src/main/java/WebProject01/WebTest01/App.java
@@ -0,0 +1,13 @@
+package WebProject01.WebTest01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
